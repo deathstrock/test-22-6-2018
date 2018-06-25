6781087a976d7fcefd504c284de7f8343dda8403
deathstrock <39049658+deathstrock@users.noreply.github.com>
2018-06-25 18:04:39 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-ssh -i RHEL_Apache_key.pem ec2-user@ip-100-103-138-62.ap-northeast-1.compute.internal:22
+ssh -i RHEL_Apache_key.pem ec2-user@ip-100-103-138-62.ap-northeast-1.compute.internal
 ls
 pwd
