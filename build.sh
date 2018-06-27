e620480bdae9a3c7de7410a94d30ac2bbecd6c5d
root <root@ip-100-103-139-206.apne1.poc.aws.asurion.net>
2018-06-27 07:10:43 +0000

    kdk

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ssh -i RHEL_Apache_key.pem -o "StrictHostKeyChecking no" ec2-user@100.103.138.62
+ssh -i key.pem -o "StrictHostKeyChecking no" ec2-user@100.103.138.62
 yum install httpd -y
 systemctl start httpd
 systemctl enable httpd
